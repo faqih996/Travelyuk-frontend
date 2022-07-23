035884a3926b36c7c50a60f3481ab4e4ebff8bbd
Faqih996 <faqih.syakir11@gmail.com>
2022-07-24 00:25:12 +0700

    Travelyuk initialization

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+  return (
+    <>
+      <div className="text-primary-blue font-label text-2xl text-bold">Hello, Travelo!</div>
+      <div className="text-primary-blue font-sans text-2xl text-bold">Hello, Travelo!</div>
+    </>
+    
+  )
+}
