071db82d7ae7ea12829d619d3a4b6a96d49a50ec
Faqih996 <faqih.syakir11@gmail.com>
2022-08-09 23:17:44 +0700

    Login page

---
+++ src/containers/Login/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "components/Layout"
+
+import styles from "./Login.module.css"
+
+const Login = () => {
+    return(
+        <Layout noFooter>
+
+        </Layout>
+
+    )
+}
+
+export default Login
